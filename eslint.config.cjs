e8167d7d163d57dcac09440c7a89c78fc09cd2fc
Gho0sted <qwertyuiop62626269@gmail.com>
2025-06-27 12:00:03 +0300

    Simplify ESLint setup

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,37 +1,23 @@
-const { FlatCompat } = require("@eslint/eslintrc");
-
-// Adapter for classic shareable configs
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-module.exports = [
-  // Apply recommended presets via FlatCompat
-  ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:security/recommended",
-    "plugin:prettier/recommended",
-  ),
-  {
-    plugins: {
-      react: require("eslint-plugin-react"),
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-      security: require("eslint-plugin-security"),
-      prettier: require("eslint-plugin-prettier"),
-    },
-    languageOptions: {
-      parser: require("@typescript-eslint/parser"),
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname,
-      },
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      "no-console": "warn",
-      "prettier/prettier": "error",
-    },
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'security', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:security/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-];
+  settings: {
+    react: { version: 'detect' }
+  },
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': 'error'
+  }
+};
