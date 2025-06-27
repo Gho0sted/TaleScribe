015d06277092749b3e1c141ed00372f1f7f74ea5
Gho0sted <qwertyuiop62626269@gmail.com>
2025-06-27 11:32:46 +0300

    Add Prettier plugin and update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,26 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:security/recommended',
-    'plugin:prettier/recommended'
-  ],
-  plugins: ['react', 'security', '@typescript-eslint'],
-  settings: { react: { version: 'detect' } },
-  rules: {
-    'no-console': 'warn',
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+module.exports = [
+  ...compat.config({
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:security/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', 'security', '@typescript-eslint'],
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'no-console': 'warn'
+    }
+  }),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
       parserOptions: { project: './tsconfig.json' }
     }
-  ]
-};
+  }
+];
